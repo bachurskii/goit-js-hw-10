add7cc8d17c83ac8b6fd82bcd1e686d965f315c3
bachurskii <123319264+bachurskii@users.noreply.github.com>
2023-06-05 10:07:05 +0000

    Deploying to gh-pages from @ bachurskii/goit-js-hw-10@a16a856242a02fa6f4817e90fc547eb5fd7040f9 🚀

---
+++ index.72ede4f2.js
@@ -0,0 +1,2 @@
+fetch("live_bv9tvTXXqygEE2ubZ5UKLEE9Lmkq0wz3Ra3ATvVTOQJdAbapdHqdv3QXhO5o0alu");
+//# sourceMappingURL=index.72ede4f2.js.map
---
+++ index.a33a8838.js
@@ -0,0 +1,2 @@
+fetch("live_bv9tvTXXqygEE2ubZ5UKLEE9Lmkq0wz3Ra3ATvVTOQJdAbapdHqdv3QXhO5o0alu");
+//# sourceMappingURL=index.a33a8838.js.map
